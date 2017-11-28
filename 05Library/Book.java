e9d24d61a50add68c794867a24aa61ed1b758668
Jason <mickeu420@gmail.com>
2017-11-28 14:38:31 -0500

    added Book.java

---
+++ 05Library/Book.java
@@ -0,0 +1,37 @@
+public class Book{
+    private String author,title,isbin;
+    
+    public Book(){
+	author = "Donald E. Knuth";
+	title = "The Art of Computer Programming";
+	isbin = "9780201311204";
+    }
+
+    public Book(String a,String t,String i){
+	author = a;
+	title = t;
+	isbin = i;
+    }
+
+    public String getAuthor(){
+	return author;
+    }
+    public String getTitle(){
+	return title;
+    }
+    public String getISBIN(){
+	return isbin;
+    }
+
+    public void setAuthor(a){
+        author=a;
+    }
+    public void setTitle(a){
+        title=a;
+    }
+    public void setISBIN(a){
+        isbin=a;
+    }
+    
+    
+}
