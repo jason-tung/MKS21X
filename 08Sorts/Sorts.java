4d7a52ed42f6f887435e5196b08efe7961891b5b
jason <mickeu420@gmail.com>
2017-12-07 02:29:50 -0500

    dafafa

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,47 +1,19 @@
 import java.util.*;
 public class Sorts{
-  /***
+
   public static void main(String[]args){
-    int [] data1 = {8,6,3,4,5,9,1,0,2,2,5,3,9,8,7},
-      data2 = {0,1,2,3,4,5},
-      data3 = {},
-      data4 = {9,8,7,6,5,4,3,2,1,0},
-      data5 = {-1,-2,-3,-2,-5,6,8,5};
-  //selection sort
-    Sorts.selectionSort(data1);
-    Sorts.selectionSort(data2);
-    Sorts.selectionSort(data3);
-    Sorts.selectionSort(data4);
-    Sorts.selectionSort(data5);
-    System.out.println(d(data1));
-    System.out.println(d(data2));
-    System.out.println(d(data3));
-    System.out.println(d(data4));
-    System.out.println(d(data5));
-  }
-  ***/
-
-  public static String d(int[] s){
-    String str = "[";
-    for (int x: s){
-      str += x + ",";
+    int[] poopypants = new int[10];
+    for (int i =0; i < 10; i++){
+      poopypants[i]=(int)(Math.random()*100);
     }
-    if (str.length()==1){
-        return str+"]";
-      }
-    return str.substring(0,str.length()-1) + "]";
+    int[] pp = poopypants.clone();
+    //--------------------CHANGE SORT HERE--------------------//
+    insertionSort(pp); //selectionSort(pp);
+    //--------------------CHANGE SORT HERE--------------------//
+    System.out.println(d(pp));
+    Arrays.sort(poopypants);
+    System.out.println(Arrays.equals(pp,poopypants));
   }
-
-  public static String name(){
-    return "10.Tung.Jason";
-  }
-  /*
-  public static void switcheroni(int a, int b){
-    int c = a;
-    a = b;
-    b = c;
-  }
-  */
 
   public static void selectionSort(int[] data){
     for (int i = 0; i < data.length; i++){
@@ -58,5 +30,66 @@ public class Sorts{
       }
     }
   }
+
+  public static void insertionSort(int[] data){
+    for (int i = 0; i < data.length; i++){
+      int minIndex = i;
+      for (int j = i-1; j >= 0;j--){
+        if (data[j] < data[minIndex]){
+          minIndex = j;
+        }
+      }
+      if (minIndex!=i){
+        int hol = data[i];
+        data[i] = data[minIndex];
+        data[minIndex] = hol;
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public static String d(int[] s){
+    String str = "[";
+    for (int x: s){
+      str += x + ",";
+    }
+    if (str.length()==1){
+      return str+"]";
+    }
+    return str.substring(0,str.length()-1) + "]";
+  }
+
+  public static String name(){
+    return "09.Tung.Jason";
+  }
+
+  /*
+    public static void switcheroni(int a, int b){
+    int c = a;
+    a = b;
+    b = c;
+    }
+  */
 
 }
