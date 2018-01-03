c3ff1cc0ad519e463cb9ba476f1035c6e2825cc4
Jason <mickeu420@gmail.com>
2018-01-03 14:46:33 -0500

    automated commit and push

--- 02OrderedSuperArray/OrderedSuperArray.java
+++ 02OrderedSuperArray/OrderedSuperArray.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class OrderedSuperArray extends SuperArray{
   public OrderedSuperArray(){
     super(10);
@@ -11,6 +12,129 @@ public class OrderedSuperArray extends SuperArray{
       add(x);
     }
   }
+    public static void main(String[] args){
+	for (int x = 0; x < 9; x ++){
+	    runTest02(x);
+	}
+	runTest02(3);
+	
+    }
+
+    
+public static void runTest02(int testID){
+  
+  if(testID<0){
+    System.out.println("Error in driver usage!");
+    System.exit(0);
+  }
+  
+  OrderedSuperArray s1 = new OrderedSuperArray();
+  ArrayList<String> s2 = new ArrayList<>();
+  
+  try{
+    if(testID == 0 ){
+    }
+    
+    if(testID == 1 ){
+      s1.add("4");
+      s2.add("4");
+      s1.add("1");
+      s2.add("1");
+      s1.add("0");
+      s2.add("0");
+    }
+    
+    if(testID == 2 ){
+      s1.add("3");
+      s2.add("3");
+      s1.add("1");
+      s2.add("1");
+      s1.add("5");
+      s2.add("5");
+      s1.add("0");
+      s2.add("0");
+    }
+    
+    if(testID == 3 ){
+      s1.add("1");
+      s2.add("1");
+      for(int i = 0; i < 10; i ++){
+        String v = ""+(int)(Math.random()*1000);
+        int in = (int)(Math.random()*s2.size());
+        s1.add(in,v);
+        s2.add(in,v);
+      }
+      System.out.println(s1);
+      System.out.println(s2);
+      
+    }
+    
+    if(testID == 4 ){
+      s1.add("1");
+      s2.add("1");
+      try{
+        s1.set(0,"");
+      } catch(UnsupportedOperationException e){
+        
+      }
+    }
+    
+    if(testID == 5 ){
+      try{
+        s1.set(0,"");
+      } catch(UnsupportedOperationException e){
+        
+      }
+    }
+    
+    if(testID == 6 ){
+      String[] x= {"adsf","b","X","C","fish","cat","Abby","break","romp"};
+      s1 = new OrderedSuperArray(x);
+      s2.addAll(Arrays.asList(x));
+    }
+    if(testID == 7 ){
+      s1.add("1");
+      s2.add("1");
+      
+      for(int i = 0; i < 1000;   i ++){
+        String v = ""+(int)(Math.random()*1000);
+        s1.add(v);
+        s2.add(v);
+      }
+    }
+    
+    
+  }catch(Exception f){
+    s2.add("0");
+    // f.printStackTrace();
+  }
+  
+  Collections.sort(s2);
+  if(equals(s1,s2)){
+    System.out.println("Test "+testID+",PASS");
+  }else{
+          System.out.println("Test "+testID+",FAIL!");// "+s1+"!="+s2);
+      System.out.println(s1);
+      System.out.println(s2);
+
+  }
+}
+
+
+//oops!
+public static boolean equals(OrderedSuperArray s, ArrayList<String> a){
+    if(s.size() == a.size()){
+      for(int i = 0; i < s.size(); i++){
+        if(!s.get(i).equals( a.get(i))){
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+
   //testing method
   //  public boolean v(int i, String value){
   //   super.add(i, value);
--- 02OrderedSuperArray/SuperArray.java
+++ 02OrderedSuperArray/SuperArray.java
@@ -1,10 +1,142 @@
 
-
+import java.util.*;
 //i forgot how to do documentation rofl
 public class SuperArray{
   //fields
   private String[] data;
-  private int size;
+    private int size;
+
+    public static void main(String[] args){
+	for (int x = 0; x < 10; x++){
+	    runTest01(x);
+	}
+    }
+    
+    public static void runTest01(int testID){
+	if(testID<0){
+	    System.out.println("Error in driver usage!");
+	    System.exit(0);
+	}
+  
+	SuperArray s1 = new SuperArray();
+	ArrayList<String> s2 = new ArrayList<>();
+  
+	try{
+	    if(testID == 0 ){
+	    }
+    
+	    if(testID == 1 ){
+		s1.add("0");
+		s2.add("0");
+		s1.add("1");
+		s2.add("1");
+	    }
+    
+	    if(testID == 2 ){
+      
+		s1.add("0");
+		s2.add("0");
+		s1.add("1");
+		s2.add("1");
+		s1.add(1,"5");
+		s2.add(1,"5");
+		s1.add(0,"6");
+		s2.add(0,"6");
+		s1.add(s1.size()-1,"4");
+		s2.add(s2.size()-1,"4");
+	    }
+    
+	    if(testID == 3 ){
+		s1.add("0");
+		s2.add("0");
+		s1.add("1");
+		s2.add("1");
+		s1.add(1,"5");
+		s2.add(1,"5");
+		s1.add(0,"6");
+		s2.add(0,"6");
+		s1.add(s1.size(),"4");
+		s2.add(s2.size(),"4");
+	    }
+    
+	    if(testID == 4 ){
+		s1.add(0,"0");
+		s2.add(0,"0");
+	    }
+    
+	    if(testID == 5 ){
+		s1.add("1");
+		s2.add("1");
+		for(int i = 0; i < 10; i ++){
+		    String v = ""+(int)(Math.random()*1000);
+		    int in = (int)(Math.random()*s2.size());
+		    s1.add(in,v);
+		    s2.add(in,v);
+		}
+	    }
+    
+	    if(testID == 6 ){
+		try{
+		    s1.get(0);
+		} catch(IndexOutOfBoundsException e){
+        
+		}
+	    }
+    
+	    if(testID == 7 ){
+		try{
+		    s1.set(0,"");
+		} catch(IndexOutOfBoundsException e){
+        
+		}
+	    }
+    
+	    if(testID == 8 ){
+		try{
+		    s1.add(1,"");
+		} catch(IndexOutOfBoundsException e){
+        
+		}
+	    }
+	    if(testID == 9 ){
+		s1.add("1");
+		s2.add("1");
+      
+		for(int i = 0; i < 1000;   i ++){
+		    String v = ""+(int)(Math.random()*1000);
+		    int in = (int)(Math.random()*s2.size());
+		    s1.add(in,v);
+		    s2.add(in,v);
+		}
+	    }
+    
+    
+	}catch(Exception f){
+	    s2.add("0");
+	    //f.printStackTrace();
+	}
+  
+	if(equals(s1,s2)){
+	    System.out.println("Test "+testID+",PASS");
+	}else{
+	    System.out.println("Test "+testID+",FAIL");
+	}
+       
+  
+  
+    }
+    public static boolean equals(SuperArray s, ArrayList<String> a){
+    if(s.size() == a.size()){
+      for(int i = 0; i < s.size(); i++){
+        if(! s.get(i).equals( a.get(i))){
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
 
   //constructors
   public SuperArray(int arySize){
@@ -72,15 +204,13 @@ public class SuperArray{
   }
   public String get(int index){
     if (index < 0 || index >= size){
-      System.out.println("the index is out of range (index < 0 || index >= size())");
-      return null;
+      throw new IndexOutOfBoundsException("the index is out of range (index < 0 || index >= size())");
     }
     return data[index];
   }
   public String set(int index, String element){
     if (index < 0 || index >= size){
-      System.out.println("the index is out of range (index < 0 || index >= size())");
-      return null;
+      throw new IndexOutOfBoundsException("the index is out of range (index < 0 || index >= size())");
     }
     String cmonBruh = data[index];
     data[index]=element;
@@ -95,7 +225,7 @@ public class SuperArray{
         nray[i] = data[i];
       }
       data = nray;
-      System.out.println("DOGS");
+      //System.out.println("DOGS");
     }
     else{
       String[] nray = new String[data.length*2];
@@ -131,7 +261,7 @@ public class SuperArray{
   }
   public void add(int index, String element){
     if (index < 0 || index > size){
-      System.out.println("the index is out of range (index < 0 || index > size())");
+       throw new IndexOutOfBoundsException("the index is out of range (index < 0 || index >= size())");
     }
     else{
       while (size == data.length || index >= data.length){
@@ -159,8 +289,7 @@ public class SuperArray{
   }
   public String remove(int index){
     if (index < 0 || index >= size){
-      System.out.println("the index is out of range (index < 0 || index >= size())");
-      return null;
+       throw new IndexOutOfBoundsException("the index is out of range (index < 0 || index >= size())");
     }
     String target;
     String[] nray = new String[size];
